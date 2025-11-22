1d3ae75174bc9fb95dc4259ac0ed5f66d5b26ce4
navarro-michelle <michelle732.mn@gmail.com>
2025-11-22 06:41:22 -0600

    refactor: update constructor to use super.key shorthand

--- lib/screens/image_upload_screen.dart
+++ lib/screens/image_upload_screen.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
 class ImageUploadScreen extends StatefulWidget {
-  const ImageUploadScreen({Key? key}) : super(key: key);
+  const ImageUploadScreen({super.key});
 
   @override
   State<ImageUploadScreen> createState() => _ImageUploadScreenState();
